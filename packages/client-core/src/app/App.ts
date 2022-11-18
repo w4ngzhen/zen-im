91ad4ced855b12d535e2154e02c32cd4ab2d037b
w4ngzhen <w4ngzhen@hotmail.com>
2022-11-18 16:02:24 +0800

    create app and export.

---
+++ packages/client-core/src/app/App.ts
@@ -0,0 +1,76 @@
+import {LoginService} from "../service/login/LoginService";
+import {ChatSessionService} from "../service/chat-session/ChatSessionService";
+import {UserService} from "../service/user/UserService";
+import {EventBus, createEventBus} from "../event/EventBus";
+import {Service} from "../service/Service";
+import {LoginEventType} from "../event/type/LoginEventType";
+import {API_MANAGER} from "../api/ApiManager";
+
+export class App {
+
+    private readonly _eventBus: EventBus;
+
+    private readonly _loginService: LoginService;
+    private readonly _chatSessionService: ChatSessionService;
+    private readonly _userService: UserService;
+
+    private get services(): Service[] {
+        return [
+            this._loginService,
+            this._userService,
+            this._chatSessionService
+        ]
+    }
+
+    constructor(baseServiceUrl: string) {
+        API_MANAGER.init(baseServiceUrl);
+        this._eventBus = createEventBus();
+        this._loginService = new LoginService(this._eventBus);
+        this._chatSessionService = new ChatSessionService(this._eventBus);
+        this._userService = new UserService(this._eventBus);
+    }
+
+    private init() {
+        // 登录事件监听
+        this._eventBus.on(LoginEventType.loginConnected, async () => {
+            await this.onAfterLogin();
+        });
+        this._eventBus.on(LoginEventType.logout, async () => {
+            await this.onAfterLogout();
+        });
+    }
+
+    private async onAppStart(): Promise<void> {
+        this.init();
+        for (let service of this.services) {
+            await service.onAppStart();
+        }
+    }
+
+    private async onAfterLogin(): Promise<void> {
+        for (let service of this.services) {
+            await service.onAfterLogin();
+        }
+    }
+
+    private async onAfterLogout(): Promise<void> {
+        for (let service of this.services) {
+            await service.onAfterLogout();
+        }
+    }
+
+    private async onAppExit(): Promise<void> {
+        for (let service of this.services) {
+            await service.onAppExit();
+        }
+    }
+
+    async start() {
+        await this.onAppStart();
+    }
+
+    async exit() {
+        await this.onAppExit();
+    }
+
+}
