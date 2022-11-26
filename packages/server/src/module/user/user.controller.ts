1dca7bef0c02d39b48767276bd1317e82e9b8d95
w4ngzhen <w4ngzhen@hotmail.com>
2022-11-26 21:51:26 +0800

    add: 新增获取当前用户API

--- packages/server/src/module/user/user.controller.ts
+++ packages/server/src/module/user/user.controller.ts
@@ -1,5 +1,10 @@
-import {Controller, Get, Query} from "@nestjs/common";
+import {Controller, Get, Query, Req} from "@nestjs/common";
 import {UserService} from "./user.service";
+import * as _ from 'lodash';
+import {ImException} from "../../base/im-exception";
+import {ERR_AUTH_ERROR} from "@zen-im/common";
+import {decodeFromToken} from "../../utils/token-utils";
+import {Request} from 'express';
 
 @Controller("users")
 export class UserController {
@@ -9,5 +14,15 @@ export class UserController {
     @Get()
     async getUserById(@Query('id') id: string) {
         return this.userService.getUserById(id);
+    }
+
+    @Get('current')
+    async getCurrentUser(@Req() httpReq: Request) {
+        const token = httpReq.headers['token'] as string;
+        if (_.isEmpty(token)) {
+            throw ImException.create(ERR_AUTH_ERROR, 'token is empty');
+        }
+        const userId = decodeFromToken(token);
+        return this.userService.getUserById(userId);
     }
 }
